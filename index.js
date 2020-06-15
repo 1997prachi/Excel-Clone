6c81b53b3be2bcd69c4e3c0c8dd42ffff5a0ca40
1997prachi <prachi.princy97@gmail.com>
2020-06-16 00:37:26 +0530

    adding some cool features

--- index.js
+++ index.js
@@ -1,16 +1,73 @@
 const $ = require("jquery");
+const electron = require('electron').remote;
+const dialog = electron.dialog;
+const fsp = require('fs').promises;
 
 $(document).ready(function () {
     $('#content-container').on('scroll', function () {
-        $('#first-row').css('top', $('#content-container').scrollTop());
-        $('#first-col').css('top', $('#content-container').scrollLeft());
         $('#tl-cell').css('top', $('#content-container').scrollTop());
-        $('#tl-cell').css('top', $('#content-container').scrollLeft());
+        $('#tl-cell').css('left', $('#content-container').scrollLeft());
+        $('#first-row').css('top', $('#content-container').scrollTop());
+        $('#first-col').css('left', $('#content-container').scrollLeft());
     });
-   
+
+    $("#new").on('click', function () {
+        $('#grid').find('.row').each(function () {
+            $(this).find('.cell').each(function () {
+                $(this).html('');
+
+            })
+        })
+
+    })
+    $("#open").on('click', async function () {
+        let dobj = await dialog.showOpenDialog();
+        if (dobj.canceled) {
+            return;
+        }
+        else if (dobj.filePaths.length === 0) {
+            alert("Please select a file");
+            return;
+        }
+        else {
+            let data = await fsp.readFile(dobj.filePaths[0]);
+            let rows = JSON.parse(data);
+            let i = 0;
+            $('#grid').find('.row').each(function () {
+                let j = 0;
+                $(this).find('.cell').each(function () {
+                    $(this).html(rows[i][j]);
+                    j++;
+                })
+                i++;
+            })
+        }
+
+    })
+    $("#save").on('click', async function () {
+        let rows = [];
+        $('#grid').find('.row').each(function () {
+            let cells = [];
+            $(this).find('.cell').each(function () {
+                cells.push($(this).html());
+
+            })
+            rows.push(cells);
+        })
+        let dobj = await dialog.showSaveDialog();
+        if (dobj.cancelled) {
+            return;
+        }
+        else if (dobj.filePath === '') {
+            alert("Please Select a file");
+            return;
+        }
+        else {
+            await fsp.writeFile(dobj.filePath, JSON.stringify(rows))
+            alert("Saved Succesfully");
+        }
+    })
+
+
+
 })
-
-
-
-
-
